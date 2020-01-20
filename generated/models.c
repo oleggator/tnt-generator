1b0582941444dd2e1a9f3d546ec7260d3df11cdc
Oleg Utkin <o.utkin@corp.mail.ru>
2020-01-20 12:08:20 +0300

    update example

--- generated/models.c
+++ generated/models.c
@@ -1,54 +1,95 @@
 
-#include <stdint.h>
 #include <msgpuck.h>
+#include <stdint.h>
 #include <stdio.h>
 
 #include "models.h"
 
-
-
-int encode_with_arr(with_arr_t * with_arr,
-	char ** buf, char ** buf_end)
-{
-	// field arr
+int encode_with_arr(with_arr_t *with_arr, char **buf, char **buf_end) {
+  // field arr
 }
 
+int decode_with_arr(with_arr_t *with_arr, const char **buf,
+                    const char **buf_end) {
+  int err = 0;
 
-int decode_with_arr(with_arr_t * with_arr,
-	const char ** buf, const char ** buf_end)
-{
-	// field arr
+  uint32_t field_count = mp_decode_array(buf);
+  if (field_count != 1) {
+    goto wrong_field_count_error;
+  }
+  // field arr
+
+  with_arr->arr_len = mp_decode_array(buf);
+  for (uint32_t i = 0; i < with_arr->arr_len; ++i) {
+    with_arr->arr[i] = mp_decode_int(buf);
+  }
+
+  return 0;
+
+wrong_field_count_error:
+  /*	say_error("wrong '%s' fields count - %d, must be %d", ".Name",
+   * field_count, 1);*/
+  return 1;
+too_big_array:
+  return 2;
 }
 
-
-
-int encode_hello_request(hello_request_t * hello_request,
-	char ** buf, char ** buf_end)
-{
-	// field with_arr
-	// field greeting
+int encode_hello_request(hello_request_t *hello_request, char **buf,
+                         char **buf_end) {
+  // field with_arr
+  // field greeting
 }
 
+int decode_hello_request(hello_request_t *hello_request, const char **buf,
+                         const char **buf_end) {
+  int err = 0;
 
-int decode_hello_request(hello_request_t * hello_request,
-	const char ** buf, const char ** buf_end)
-{
-	// field with_arr
-	// field greeting
+  uint32_t field_count = mp_decode_array(buf);
+  if (field_count != 2) {
+    goto wrong_field_count_error;
+  }
+  // field with_arr
+
+  err = decode_with_arr(&hello_request->with_arr, buf, buf_end);
+  if (err != 0) {
+    return err;
+  };
+
+  // field greeting
+  hello_request->greeting = mp_decode_str(buf, &hello_request->greeting_len);
+
+  return 0;
+
+wrong_field_count_error:
+  /*	say_error("wrong '%s' fields count - %d, must be %d", ".Name",
+   * field_count, 2);*/
+  return 1;
+too_big_array:
+  return 2;
 }
 
-
-
-int encode_hello_response(hello_response_t * hello_response,
-	char ** buf, char ** buf_end)
-{
-	// field reply
+int encode_hello_response(hello_response_t *hello_response, char **buf,
+                          char **buf_end) {
+  // field reply
 }
 
+int decode_hello_response(hello_response_t *hello_response, const char **buf,
+                          const char **buf_end) {
+  int err = 0;
 
-int decode_hello_response(hello_response_t * hello_response,
-	const char ** buf, const char ** buf_end)
-{
-	// field reply
+  uint32_t field_count = mp_decode_array(buf);
+  if (field_count != 1) {
+    goto wrong_field_count_error;
+  }
+  // field reply
+  hello_response->reply = mp_decode_str(buf, &hello_response->reply_len);
+
+  return 0;
+
+wrong_field_count_error:
+  /*	say_error("wrong '%s' fields count - %d, must be %d", ".Name",
+   * field_count, 1);*/
+  return 1;
+too_big_array:
+  return 2;
 }
-
--- generated/models.h
+++ generated/models.h
@@ -2,105 +2,93 @@
 #pragma once
 
 #include <stdint.h>
+#include <tarantool/module.h>
 
 #define ARRAY_LEN 1024
 
-
-    
 typedef struct {
-	int32_t arr[ARRAY_LEN];
-	uint32_t arr_len;
+  int64_t arr[ARRAY_LEN];
+  uint32_t arr_len;
 } with_arr_t;
 
-
-    
 /* with_arr struct encoder
  *
- * @param	with_arr		struct to encode 	
+ * @param	with_arr		struct to encode
  * @param	buf					data buffer
  * @param	buf_end				data buffer end
  *
  * @return	result code
  */
-    
-int encode_with_arr(with_arr_t * with_arr,
-	char ** buf, char ** buf_end);
 
-    
+int encode_with_arr(with_arr_t *with_arr, char **buf, char **buf_end);
+
 /* with_arr struct decoder
  *
- * @param	with_arr		struct to decode 	
- * @param	buf					data buffer
- * @param	buf_end				data buffer end
+ * @param	with_arr        struct to decode
+ * @param	buf             data buffer
+ * @param	buf_end         data buffer end
  *
  * @return	result code
  */
-    
-int decode_with_arr(with_arr_t * with_arr,
-	const char ** buf, const char ** buf_end);
 
-    
+int decode_with_arr(with_arr_t *with_arr, const char **buf,
+                    const char **buf_end);
+
 typedef struct {
-	with_arr_t with_arr;
-	char * greeting;
+  with_arr_t with_arr;
+  const char *greeting;
+  uint32_t greeting_len;
 } hello_request_t;
 
-
-    
 /* hello_request struct encoder
  *
- * @param	hello_request		struct to encode 	
+ * @param	hello_request		struct to encode
  * @param	buf					data buffer
  * @param	buf_end				data buffer end
  *
  * @return	result code
  */
-    
-int encode_hello_request(hello_request_t * hello_request,
-	char ** buf, char ** buf_end);
 
-    
+int encode_hello_request(hello_request_t *hello_request, char **buf,
+                         char **buf_end);
+
 /* hello_request struct decoder
  *
- * @param	hello_request		struct to decode 	
- * @param	buf					data buffer
- * @param	buf_end				data buffer end
+ * @param	hello_request   struct to decode
+ * @param	buf             data buffer
+ * @param	buf_end         data buffer end
  *
  * @return	result code
  */
-    
-int decode_hello_request(hello_request_t * hello_request,
-	const char ** buf, const char ** buf_end);
 
-    
+int decode_hello_request(hello_request_t *hello_request, const char **buf,
+                         const char **buf_end);
+
 typedef struct {
-	char * reply;
+  const char *reply;
+  uint32_t reply_len;
 } hello_response_t;
 
-
-    
 /* hello_response struct encoder
  *
- * @param	hello_response		struct to encode 	
+ * @param	hello_response		struct to encode
  * @param	buf					data buffer
  * @param	buf_end				data buffer end
  *
  * @return	result code
  */
-    
-int encode_hello_response(hello_response_t * hello_response,
-	char ** buf, char ** buf_end);
 
-    
+int encode_hello_response(hello_response_t *hello_response, char **buf,
+                          char **buf_end);
+
 /* hello_response struct decoder
  *
- * @param	hello_response		struct to decode 	
- * @param	buf					data buffer
- * @param	buf_end				data buffer end
+ * @param	hello_response  struct to decode
+ * @param	buf             data buffer
+ * @param	buf_end         data buffer end
  *
  * @return	result code
  */
-    
-int decode_hello_response(hello_response_t * hello_response,
-	const char ** buf, const char ** buf_end);
 
+int decode_hello_response(hello_response_t *hello_response, const char **buf,
+                          const char **buf_end);
